2ff122ec535b09abc48e4a3f2b756816d1642284
knxxxn <20211308@sungshin.ac.kr>
2023-10-17 20:20:57 +0900

    xml만들기 -1

---
+++ app/src/main/java/com/example/myreading/Bookpage.kt
@@ -0,0 +1,11 @@
+package com.example.myreading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bookpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookpage)
+    }
+}
